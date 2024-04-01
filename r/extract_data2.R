24a1ef3a3502f9b99ace11b2815f718751db30cd
akmiller01 <alex.k.miller@gmail.com>
2024-04-01 14:19:30 -0400

    Try large multilabel

---
+++ r/extract_data2.R
@@ -0,0 +1,135 @@
+list.of.packages <- c("data.table", "openxlsx", "tidyverse", "Hmisc")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only=T)
+rm(list.of.packages,new.packages)
+
+setwd("~/git/cdp-paf/")
+
+### Load ####
+paf = read.xlsx("large_data/Full PAF dataset.xlsx", sheet="PAF", na.strings = c("", "#N/A"))
+paf = paf[which(!duplicated(paf)),]
+crisis = read.xlsx("large_data/Full PAF dataset.xlsx", sheet="Total crisis financing", na.strings = c("", "#N/A"))
+crisis = crisis[which(!duplicated(crisis)),]
+aa = read.xlsx("large_data/Full PAF dataset.xlsx", sheet="Funding for AA", na.strings = c("", "#N/A"))
+aa = aa[which(!duplicated(aa)),]
+hum = read.xlsx("large_data/Full PAF dataset.xlsx", sheet="Humanitarian funding", na.strings = c("", "#N/A"))
+hum = hum[which(!duplicated(hum)),]
+dat = fread("./large_data/iati-text-enhanced-crs.csv")
+dat$long = dat$iati_match_type=="identifier" |
+  (dat$iati_match_type=="project title" & nchar(dat$ProjectTitle) > 20) |
+  (dat$iati_match_type=="short description" & nchar(dat$ShortDescription) > 20) |
+  (dat$iati_match_type=="long description" & nchar(dat$LongDescription) > 20)
+sum(dat$iati_text=="")
+dat$iati_text[which(!dat$long)] = ""
+sum(dat$iati_text=="")
+dat$long = NULL
+dat = dat[which(!duplicated(dat)),]
+
+### Join metadata from CDP files ####
+common_names = names(crisis)
+common_names = names(dat)[which(names(dat) %in% common_names)]
+crs = dat[,c(common_names, "iati_text"), with=F]
+
+### Join/process Crisis ####
+crisis = crisis[,common_names]
+crisis$crisis_label = "Crisis financing"
+crs = merge(crs, crisis, by=common_names, all=T)
+
+### Join/process PAF ####
+paf$crisis_label = "Crisis financing"
+
+paf$paf_label = "PAF"
+
+paf$direct_label = NA
+paf$direct_label[which(paf$Type.of.PAF=="Direct")] = "Direct"
+paf$direct_label[which(paf$Type.of.PAF=="Indirect")] = "Indirect"
+paf$direct_label[which(paf$Type.of.PAF=="Both")] = "Direct,Indirect"
+paf$direct_label[which(paf$Type.of.PAF=="Part PAF")] = "Part"
+
+paf$ddo_label = NA
+paf$ddo_label[which(paf$WB.Cat.DDO=="Yes")] = "WB CAT DDO"
+
+paf$cf_label = NA
+paf$cf_label[which(paf$Contingent.financing=="Yes")] = "Contingent financing"
+
+paf = paf[,c(common_names, "crisis_label", "paf_label", "direct_label", "ddo_label", "cf_label")]
+crs = merge(crs, paf, by=common_names, all=T)
+diff = subset(crs, crisis_label.x!=crisis_label.y)
+stopifnot({nrow(diff)==0})
+crs$crisis_label.x[which(is.na(crs$crisis_label.x))] = crs$crisis_label.y[which(is.na(crs$crisis_label.x))]
+crs$crisis_label.y = NULL
+setnames(crs, "crisis_label.x", "crisis_label")
+
+### Join/process AA ####
+aa$crisis_label = "Crisis financing"
+
+aa$paf_label = "PAF"
+
+aa$aa_label = "AA"
+
+aa$direct_label = NA
+aa$direct_label[which(aa$Type.oF.AF=="Direct")] = "Direct"
+aa$direct_label[which(aa$Type.oF.AF=="Indirect")] = "Indirect"
+aa$direct_label[which(aa$Type.oF.AF=="Both")] = "Direct,Indirect"
+aa$direct_label[which(aa$Type.oF.AF=="Part PAF")] = "Part"
+
+aa = aa[,c(common_names, "crisis_label", "paf_label", "aa_label", "direct_label")]
+crs = merge(crs, aa, by=common_names, all=T)
+diff = subset(crs, crisis_label.x!=crisis_label.y)
+stopifnot({nrow(diff)==0})
+diff = subset(crs, paf_label.x!=paf_label.y)
+stopifnot({nrow(diff)==0})
+diff = subset(crs, direct_label.x!=direct_label.y)
+stopifnot({nrow(diff)==0})
+crs$crisis_label.x[which(is.na(crs$crisis_label.x))] = crs$crisis_label.y[which(is.na(crs$crisis_label.x))]
+crs$crisis_label.y = NULL
+crs$paf_label.x[which(is.na(crs$paf_label.x))] = crs$paf_label.y[which(is.na(crs$paf_label.x))]
+crs$paf_label.y = NULL
+crs$direct_label.x[which(is.na(crs$direct_label.x))] = crs$direct_label.y[which(is.na(crs$direct_label.x))]
+crs$direct_label.y = NULL
+setnames(crs, "crisis_label.x", "crisis_label")
+setnames(crs, "paf_label.x", "paf_label")
+setnames(crs, "direct_label.x", "direct_label")
+
+### Concatenate textual columns ####
+textual_cols_for_classification = c(
+  "DonorName",
+  "ProjectTitle",
+  "SectorName",
+  "PurposeName",
+  "FlowName",
+  # "ChannelName",
+  "ShortDescription",
+  "LongDescription",
+  "iati_text"
+)
+textual_cols_for_classification %in% names(crs)
+
+crs = crs %>%
+  unite(text, all_of(textual_cols_for_classification), sep=" ", na.rm=T)
+
+### Make labels ####
+label_cols = c(
+  "crisis_label",
+  "paf_label",
+  "direct_label",
+  "ddo_label",
+  "cf_label",
+  "aa_label"
+)
+
+crs = crs %>%
+  unite(labels, all_of(label_cols), sep=",", na.rm=T)
+
+crs$labels[which(crs$labels=="")] = "Unrelated"
+crs = crs[,c("text","labels")]
+crs = unique(crs)
+
+fwrite(crs, "./large_data/meta_model_data.csv")
+
+### Examine lengths ####
+context_window = 512
+text_split = strsplit(crs$text, split=" ")
+length(which(sapply(text_split, length)>context_window))/nrow(crs)
+hist(sapply(text_split, length))
--- upload_dataset.py
+++ upload_dataset.py
@@ -1,59 +1,20 @@
 from huggingface_hub import login
-from datasets import Dataset, concatenate_datasets
+from datasets import load_dataset, concatenate_datasets
 from dotenv import load_dotenv
 import pandas as pd
 import os
-from tqdm import tqdm
-from Levenshtein import distance
-
-
-def drop_similar_rows(df, text_column, threshold_percentage):
-    to_drop = set()
-    texts = df[text_column].tolist()
-    num_texts = len(texts)
-
-    for i in tqdm(range(num_texts)):
-        if i not in to_drop:
-            for j in range(i + 1, num_texts):
-                if j not in to_drop:
-                    len_i = len(texts[i])
-                    len_j = len(texts[j])
-                    threshold = max(max(len_i, len_j) * threshold_percentage, 1)
-                    if distance(texts[i], texts[j]) <= threshold:
-                        to_drop.add(j)
-    return df.drop(to_drop)
 
 
 def main():
-    data = pd.read_csv("./large_data/meta_model_data.csv")
+    dataset = load_dataset("csv", data_files="./large_data/meta_model_data.csv", split='train')
 
-    dataset = Dataset.from_pandas(data, preserve_index=False)
-
-    # Initial rough balance to speed up de-duplicate
-    positive = dataset.filter(lambda example: example["label"] == "PAF/AA")
-    negative = dataset.filter(lambda example: example["label"] == "Unrelated")
-    negative = negative.shuffle(seed=42)
-    negative = negative.select(range(positive.num_rows * 2))
-    dataset = concatenate_datasets([positive, negative])
-    data = pd.DataFrame(dataset)
-
-    # De-duplicate on exact lowercase match, and lowercase string distance
-    print("Pre-de-duplicate shape: ", data.shape)
-    data['text_lowercase'] = data['text'].str.lower()
-    data = data.drop_duplicates(subset=['text_lowercase'], keep='first')
-    data = data.reset_index(drop=True)
-    data = drop_similar_rows(data, 'text_lowercase', threshold_percentage=0.05)
-    print("Post-de-duplicate shape: ", data.shape)
-    data = data.drop(columns=['text_lowercase'])
-    dataset = Dataset.from_pandas(data, preserve_index=False)
-
-    # Final balance
-    positive = dataset.filter(lambda example: example["label"] == "PAF/AA")
-    print("Positive balance length: ", len(positive))
-    negative = dataset.filter(lambda example: example["label"] == "Unrelated")
-    negative = negative.shuffle(seed=42)
-    negative = negative.select(range(positive.num_rows))
-    dataset = concatenate_datasets([positive, negative])
+    # Balance
+    # positive = dataset.filter(lambda example: example["labels"] != "Unrelated")
+    # print("Positive balance length: ", len(positive))
+    # negative = dataset.filter(lambda example: example["labels"] == "Unrelated")
+    # negative = negative.shuffle(seed=42)
+    # negative = negative.select(range(positive.num_rows))
+    # dataset = concatenate_datasets([positive, negative])
 
     # Push
     dataset.push_to_hub("alex-miller/cdp-paf-meta")
