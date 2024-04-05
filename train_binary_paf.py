be4d48a4422164b3a85c7a29c757301424678f4a
akmiller01 <alex.k.miller@gmail.com>
2024-04-05 15:38:42 -0400

    Binary on llamda

---
+++ train_binary_paf.py
@@ -0,0 +1,91 @@
+from huggingface_hub import login
+
+login(token="")
+
+from datasets import load_dataset, concatenate_datasets
+from transformers import (
+    AutoTokenizer,
+    DataCollatorWithPadding,
+    AutoModelForSequenceClassification,
+    TrainingArguments,
+    Trainer
+)
+import evaluate
+import numpy as np
+
+
+unique_labels = ["Crisis finance", "PAF"]
+id2label = {i: label for i, label in enumerate(unique_labels)}
+label2id = {id2label[i]: i for i in id2label.keys()}
+
+
+def remap_binary(example):
+    example['label'] = 'PAF' if 'PAF' in example['labels'] else 'Crisis finance'
+    example['label'] = label2id[example['label']]
+    return example
+
+
+dataset = load_dataset("alex-miller/cdp-paf-meta", split="train")
+dataset = dataset.filter(lambda example: example['labels'] != 'Unrelated')
+dataset = dataset.map(remap_binary, num_proc=8, remove_columns=["labels"])
+# Rebalance
+positive = dataset.filter(lambda example: example["label"] == 1)
+negative = dataset.filter(lambda example: example["label"] == 0)
+negative = negative.shuffle(seed=42)
+negative = negative.select(range(positive.num_rows))
+dataset = concatenate_datasets([negative, positive])
+dataset = dataset.class_encode_column('label').train_test_split(
+    test_size=0.2,
+    shuffle=True,
+    seed=42
+)
+
+
+card = "alex-miller/ODABert"
+tokenizer = AutoTokenizer.from_pretrained(card, model_max_length=512)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+def preprocess_function(examples):
+    return tokenizer(examples['text'], truncation=True)
+
+
+clf_metrics = evaluate.combine(["accuracy", "f1", "precision", "recall"])
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    return clf_metrics.compute(predictions=predictions, references=labels)
+
+
+tokenized_data = dataset.map(preprocess_function, batched=True, num_proc=2)
+model = AutoModelForSequenceClassification.from_pretrained(
+    card, num_labels=len(id2label.keys()), id2label=id2label, label2id=label2id
+)
+
+training_args = TrainingArguments(
+    'cdp-paf-classifier',
+    learning_rate=1e-6, # This can be tweaked depending on how loss progresses
+    per_device_train_batch_size=20, # These should be tweaked to match GPU VRAM
+    per_device_eval_batch_size=20,
+    num_train_epochs=10,
+    weight_decay=0.01,
+    evaluation_strategy='epoch',
+    save_strategy='epoch',
+    logging_strategy='epoch',
+    load_best_model_at_end=True,
+    push_to_hub=True,
+    save_total_limit=5,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_data['train'],
+    eval_dataset=tokenized_data['test'],
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
+trainer.evaluate()
+trainer.push_to_hub()
