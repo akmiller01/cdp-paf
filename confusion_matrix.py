7b8646e1f74bbb1928eaec8296a30f13878aee52
akmiller01 <alex.k.miller@gmail.com>
2024-04-11 12:30:25 -0400

    Final confusion matrix for this iteration

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -4,6 +4,14 @@ import numpy as np
 from datasets import load_dataset
 from sklearn.metrics import multilabel_confusion_matrix
 import pandas as pd
+from scipy.special import softmax
+import math
+
+
+CF_POSITIVE_CONFIDENCE_THRESHOLD = 0.6
+PAF_POSITIVE_CONFIDENCE_THRESHOLD = 0.6
+AA_POSITIVE_CONFIDENCE_THRESHOLD = 0.6
+
 
 global TOKENIZER
 global DEVICE
@@ -20,47 +28,71 @@ MODEL_3 = AutoModelForSequenceClassification.from_pretrained("alex-miller/cdp-aa
 MODEL_3 = MODEL_3.to(DEVICE)
 
 
+def chunk_by_tokens(input_text, model_max_size=512):
+    chunks = list()
+    tokens = TOKENIZER.encode(input_text)
+    token_length = len(tokens)
+    if token_length <= model_max_size:
+        return [input_text]
+    desired_number_of_chunks = math.ceil(token_length / model_max_size)
+    calculated_chunk_size = math.ceil(token_length / desired_number_of_chunks)
+    for i in range(0, token_length, calculated_chunk_size):
+        chunks.append(TOKENIZER.decode(tokens[i:i + calculated_chunk_size]))
+    return chunks
+
+
 def calculate_correct(example):
     correct = (example['Crisis finance actual'] == example['Crisis finance predicted']) and (example['PAF actual'] == example['PAF predicted']) and (example['AA actual'] == example['AA predicted'])
     return correct
 
 
-def inference(model, inputs):
-    with torch.no_grad():
-        predictions = model(**inputs)
+def inference(model, inputs, positive_class):
+    predictions = model(**inputs)
 
-    logits = predictions.logits.cpu().numpy()[0]
+    logits = predictions.logits.cpu().detach().numpy()[0]
+    predicted_confidences = softmax(logits, axis=0)
     predicted_class_id = np.argmax(logits)
     predicted_class = model.config.id2label[predicted_class_id]
+    positive_class_id = model.config.label2id[positive_class]
+    positive_class_confidence = predicted_confidences[positive_class_id]
 
-    return predicted_class
+    return predicted_class, positive_class_confidence
 
 def map_columns(example):
-    inputs = TOKENIZER(example['text'], return_tensors="pt", truncation=True).to(DEVICE)
     example['Crisis finance actual'] = 'Crisis financing' in example['labels']
     example['PAF actual'] = 'PAF' in example['labels']
     example['AA actual'] = 'AA' in example['labels']
-    model_1_pred = inference(MODEL_1, inputs)
-    example['Crisis finance predicted'] = model_1_pred == 'Crisis finance'
-    if model_1_pred != 'Crisis finance':
-        example['PAF predicted'] = False
-        example['AA predicted'] = False
-        example['Correct'] = calculate_correct(example)
-        return example
-    model_2_pred = inference(MODEL_2, inputs)
-    example['PAF predicted'] = model_2_pred == 'PAF'
-    if model_2_pred != 'PAF':
-        example['AA predicted'] = False
-        example['Correct'] = calculate_correct(example)
-        return example
-    model_3_pred = inference(MODEL_3, inputs)
-    example['AA predicted'] = model_3_pred == 'AA'
+    predictions = {
+        'Crisis finance': [False, 0],
+        'PAF': [False, 0],
+        'AA': [False, 0]
+    }
+
+    text_chunks = chunk_by_tokens(example['text'])
+    for text_chunk in text_chunks:
+        inputs = TOKENIZER(text_chunk, return_tensors="pt", truncation=True).to(DEVICE)
+        model_1_pred, model_1_conf = inference(MODEL_1, inputs, 'Crisis finance')
+        predictions['Crisis finance'][0] = predictions['Crisis finance'][0] or ((model_1_pred == 'Crisis finance') and model_1_conf > CF_POSITIVE_CONFIDENCE_THRESHOLD)
+        predictions['Crisis finance'][1] = max(predictions['Crisis finance'][1], model_1_conf)
+        if model_1_pred == 'Crisis finance':
+            model_2_pred, model_2_conf = inference(MODEL_2, inputs, 'PAF')
+            predictions['PAF'][0] = predictions['PAF'][0] or ((model_2_pred == 'PAF') and model_2_conf > PAF_POSITIVE_CONFIDENCE_THRESHOLD)
+            predictions['PAF'][1] = max(predictions['PAF'][1], model_2_conf)
+            if model_2_pred == 'PAF':
+                model_3_pred, model_3_conf = inference(MODEL_3, inputs, 'AA')
+                predictions['AA'][0] = predictions['AA'][0] or ((model_3_pred == 'AA') and model_3_conf > AA_POSITIVE_CONFIDENCE_THRESHOLD)
+                predictions['AA'][1] = max(predictions['AA'][1], model_3_conf)
+    example['Crisis finance predicted'] = predictions['Crisis finance'][0]
+    example['Crisis finance confidence'] = predictions['Crisis finance'][1]
+    example['PAF predicted'] = predictions['PAF'][0]
+    example['PAF confidence'] = predictions['PAF'][1]
+    example['AA predicted'] = predictions['AA'][0]
+    example['AA confidence'] = predictions['AA'][1]
     example['Correct'] = calculate_correct(example)
     return example
 
 def main():
     dataset = load_dataset('alex-miller/cdp-paf-meta', split='train')
-    dataset = dataset.shuffle(seed=1337).select(range(100))
     dataset = dataset.map(map_columns)
     print(
         'Overall accuracy: {}%'.format(round(np.mean(dataset['Correct']) * 100, ndigits=2))
---
+++ r/explore_logits.R
@@ -0,0 +1,74 @@
+list.of.packages <- c("data.table")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only=T)
+
+wd = "~/git/cdp-paf/"
+setwd(wd)
+
+quick_summ_stat = function(vector){
+  p5 = quantile(vector, probs=0.05, na.rm=T)
+  p10 = quantile(vector, probs=0.1, na.rm=T)
+  p90 = quantile(vector, probs=0.9, na.rm=T)
+  p95 = quantile(vector, probs=0.95, na.rm=T)
+  s_mean = mean(vector, na.rm=T)
+  s_median = median(vector, na.rm=T)
+  s_min = min(vector, na.rm=T)
+  s_max = max(vector, na.rm=T)
+  message(
+    paste0(
+      "Min: ",
+      s_min,
+      "\nP5: ",
+      p5,
+      "\nP10: ",
+      p10,
+      "\nMean: ",
+      s_mean,
+      "\nMedian: ",
+      s_median,
+      "\nP90: ",
+      p90,
+      "\nP95: ",
+      p95,
+      "\nMax: ",
+      s_max
+    )
+  )
+}
+
+dat = fread("large_data/predicted_meta_model_data_colab.csv")
+
+quick_summ_stat(dat$`Crisis finance confidence`)
+quick_summ_stat(dat$`PAF confidence`)
+quick_summ_stat(dat$`AA confidence`)
+
+# Cut-off of ~0.6 is free
+cf_true = subset(dat, `Crisis finance actual`)
+quick_summ_stat(cf_true$`Crisis finance confidence`)
+# Min 0.63
+cf_false_positive = subset(dat, !`Crisis finance actual` & `Crisis finance predicted`)
+quick_summ_stat(cf_false_positive$`Crisis finance confidence`)
+# Min 0.52
+cf_false_negative = subset(dat, `Crisis finance actual` & !`Crisis finance predicted`)
+quick_summ_stat(cf_false_negative$`Crisis finance confidence`)
+
+# Cut-off of ~0.6 is free, 0.85 should lose 5% of true in exchange for mean of false
+paf_true = subset(dat, `PAF actual`)
+quick_summ_stat(paf_true$`PAF confidence`)
+# Min 0.63
+paf_false_positive = subset(dat, !`PAF actual` & `PAF predicted`)
+quick_summ_stat(paf_false_positive$`PAF confidence`)
+# Min 0.50
+paf_false_negative = subset(dat, `PAF actual` & !`PAF predicted`)
+quick_summ_stat(paf_false_negative$`PAF confidence`)
+
+# 0.60 is free.
+aa_true = subset(dat, `AA actual`)
+quick_summ_stat(aa_true$`AA confidence`)
+# Min 0.67
+aa_false_positive = subset(dat, !`AA actual` & `AA predicted`)
+quick_summ_stat(aa_false_positive$`AA confidence`)
+# Min 0.50
+aa_false_negative = subset(dat, `AA actual` & !`AA predicted`)
+quick_summ_stat(aa_false_negative$`AA confidence`)
