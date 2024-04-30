ba31ccc5a81b88ea6412183fc4bfeca685dd9a9a
akmiller01 <alex.k.miller@gmail.com>
2024-04-30 08:21:12 -0400

    Try weighted multiclass

---
+++ train_multiclass_weighted.py
@@ -0,0 +1,197 @@
+# ! pip install datasets evaluate transformers accelerate huggingface_hub --quiet
+
+# from huggingface_hub import login
+
+# login()
+
+from datasets import load_dataset
+from transformers import (
+    AutoTokenizer,
+    DataCollatorWithPadding,
+    TrainingArguments,
+    Trainer
+)
+import torch
+from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
+from transformers.models.bert.modeling_bert import BertForSequenceClassification
+from transformers.modeling_outputs import SequenceClassifierOutput
+import evaluate
+import numpy as np
+
+from typing import Optional, Tuple, Union
+
+
+class WeightedBertForSequenceClassification(BertForSequenceClassification):
+    def forward(
+        self,
+        input_ids: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        token_type_ids: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.Tensor] = None,
+        head_mask: Optional[torch.Tensor] = None,
+        inputs_embeds: Optional[torch.Tensor] = None,
+        labels: Optional[torch.Tensor] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Union[Tuple[torch.Tensor], SequenceClassifierOutput]:
+        r"""
+        labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
+            Labels for computing the sequence classification/regression loss. Indices should be in `[0, ...,
+            config.num_labels - 1]`. If `config.num_labels == 1` a regression loss is computed (Mean-Square loss), If
+            `config.num_labels > 1` a classification loss is computed (Cross-Entropy).
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        loss = None
+        if labels is not None:
+            if self.config.problem_type is None:
+                if self.num_labels == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels > 1 and (labels.dtype == torch.long or labels.dtype == torch.int):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
+
+            if self.config.problem_type == "regression":
+                loss_fct = MSELoss()
+                if self.num_labels == 1:
+                    loss = loss_fct(logits.squeeze(), labels.squeeze())
+                else:
+                    loss = loss_fct(logits, labels)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = CrossEntropyLoss(weight=self.class_weights, label_smoothing=self.label_smoothing)
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = BCEWithLogitsLoss(weight=self.class_weights)
+                loss = loss_fct(logits, labels)
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+dataset = load_dataset("alex-miller/cdp-paf-meta", split="train")
+
+unique_labels = [
+    "Unrelated",
+    "Crisis financing",
+    "PAF",
+    "AA",
+    "Direct",
+    "Indirect",
+    "Part",
+    "WB CAT DDO",
+    "Contingent financing"
+]
+id2label = {i: label for i, label in enumerate(unique_labels)}
+label2id = {id2label[i]: i for i in id2label.keys()}
+
+weight_list = list()
+total_rows = dataset.num_rows
+print("Weights:")
+for label in unique_labels:
+    label_rows = dataset.filter(lambda example: label in example['labels']).num_rows
+    label_weight = total_rows / label_rows
+    weight_list.append(label_weight)
+    print("{}: {}".format(label, label_weight))
+weights = torch.tensor(weight_list)
+
+dataset = dataset.add_column("class_labels", dataset['labels'])
+
+dataset = dataset.class_encode_column('class_labels').train_test_split(
+    test_size=0.2,
+    stratify_by_column="class_labels",
+    shuffle=True,
+    seed=42
+)
+
+card = "alex-miller/ODABert"
+tokenizer = AutoTokenizer.from_pretrained(card, model_max_length=512)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+def preprocess_function(example):
+    all_labels = example['labels'].split(",")
+    labels = [0. for i in range(len(unique_labels))]
+    for label in all_labels:
+        label_id = label2id[label]
+        labels[label_id] = 1.
+
+    example = tokenizer(example['text'], truncation=True)
+    example['labels'] = labels
+    return example
+
+
+clf_metrics = evaluate.combine(["accuracy", "f1", "precision", "recall"])
+
+def sigmoid(x):
+   return 1/(1 + np.exp(-x))
+
+
+def compute_metrics(eval_pred):
+   predictions, labels = eval_pred
+   predictions = sigmoid(predictions)
+   predictions = (predictions > 0.5).astype(int).reshape(-1)
+   return clf_metrics.compute(predictions=predictions, references=labels.astype(int).reshape(-1))
+
+
+tokenized_data = dataset.map(preprocess_function)
+model = WeightedBertForSequenceClassification.from_pretrained(
+    card,
+    num_labels=len(id2label.keys()), 
+    id2label=id2label,
+    label2id=label2id, 
+    problem_type="multi_label_classification"
+)
+model.class_weights = weights
+model.label_smoothing = 0.0
+
+training_args = TrainingArguments(
+    'cdp-multi-classifier',
+    learning_rate=1e-6, # This can be tweaked depending on how loss progresses
+    per_device_train_batch_size=16, # These should be tweaked to match GPU VRAM
+    per_device_eval_batch_size=16,
+    num_train_epochs=3,
+    weight_decay=0.01,
+    evaluation_strategy='epoch',
+    save_strategy='epoch',
+    logging_strategy='epoch',
+    load_best_model_at_end=True,
+    push_to_hub=True,
+    save_total_limit=5,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_data['train'],
+    eval_dataset=tokenized_data['test'],
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
+trainer.push_to_hub()
