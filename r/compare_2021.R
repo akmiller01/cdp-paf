289b3c63e713507538191e7ecde6138f8085fc36
akmiller01 <alex.k.miller@gmail.com>
2024-05-17 14:32:38 -0400

    Gen and compare

--- r/compare_2021.R
+++ r/compare_2021.R
@@ -3,6 +3,8 @@ new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"
 if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only=T)
 
+YEAR = 2017
+
 wd = "~/git/cdp-paf/"
 setwd(wd)
 
@@ -29,7 +31,7 @@ fix_pn = function(project_numbers){
   collapse_whitespace(trimws(remove_punct(tolower(conv_ascii(remove_lead_zero_if_number(trimws(project_numbers)))))))
 }
 
-auto = fread("large_data/crs_2021_cdp_automated.csv")
+auto = fread(paste0("large_data/crs_",YEAR,"_cdp_automated.csv"))
 auto$project_number = fix_pn(auto$project_number)
 all_pn = unique(auto$project_number)
 
@@ -38,13 +40,13 @@ aa_auto = subset(auto, `AA determination` %in% c("Yes", "Review"))
 cf_auto = subset(auto, `Crisis finance determination` %in% c("Yes", "Review"))
 
 paf = read.xlsx("large_data/Full PAF dataset.xlsx", sheet="PAF", na.strings = c("", "#N/A"))
-paf = subset(paf, Year==2021)
+paf = subset(paf, Year==YEAR)
 paf$ProjectNumber = fix_pn(paf$ProjectNumber)
 cf = read.xlsx("large_data/Full PAF dataset.xlsx", sheet="Total crisis financing", na.strings = c("", "#N/A"))
-cf = subset(cf, Year==2021)
+cf = subset(cf, Year==YEAR)
 cf$ProjectNumber = fix_pn(cf$ProjectNumber)
 aa = read.xlsx("large_data/Full PAF dataset.xlsx", sheet="Funding for AA", na.strings = c("", "#N/A"))
-aa = subset(aa, Year==2021)
+aa = subset(aa, Year==YEAR)
 aa$ProjectNumber = fix_pn(aa$ProjectNumber)
 
 # AA
@@ -59,8 +61,8 @@ missed_aa = subset(aa, ProjectNumber %in% missed_aa_pn)
 new_aa = subset(aa_auto, project_number %in% new_aa_pn)
 new_aa = new_aa[order(-new_aa$`AA confidence ML`),c("project_title","short_description","long_description","AA determination","AA confidence ML")]
 describe(new_aa$`AA determination`)
-fwrite(missed_aa, "data/missed_2021_aa.csv")
-fwrite(new_aa, "data/new_2021_aa.csv")
+fwrite(missed_aa, paste0("data/missed_",YEAR,"_aa.csv"))
+fwrite(new_aa, paste0("data/new_",YEAR,"_aa.csv"))
 
 # PAF
 paf_pn = unique(paf$ProjectNumber)
@@ -76,8 +78,8 @@ missed_paf = subset(paf, ProjectNumber %in% missed_paf_pn)
 new_paf = subset(paf_auto, project_number %in% new_paf_pn)
 new_paf = new_paf[order(-new_paf$`PAF confidence ML`),c("project_title","short_description","long_description","PAF determination","PAF confidence ML")]
 describe(new_paf$`PAF determination`)
-fwrite(missed_paf, "data/missed_2021_paf.csv")
-fwrite(new_paf, "data/new_2021_paf.csv")
+fwrite(missed_paf, paste0("data/missed_",YEAR,"_paf.csv"))
+fwrite(new_paf, paste0("data/new_",YEAR,"_paf.csv"))
 
 # CF
 cf_pn = unique(cf$ProjectNumber)
@@ -93,5 +95,5 @@ missed_cf = subset(cf, ProjectNumber %in% missed_cf_pn)
 new_cf = subset(cf_auto, project_number %in% new_cf_pn)
 new_cf = new_cf[order(-new_cf$`Crisis finance confidence ML`),c("project_title","short_description","long_description","Crisis finance determination","Crisis finance confidence ML")]
 describe(new_cf$`Crisis finance determination`)
-fwrite(missed_cf, "data/missed_2021_cf.csv")
-fwrite(new_cf, "data/new_2021_cf.csv")
+fwrite(missed_cf, paste0("data/missed_",YEAR,"_cf.csv"))
+fwrite(new_cf, paste0("data/new_",YEAR,"_cf.csv"))
